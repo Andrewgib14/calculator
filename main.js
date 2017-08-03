defc6a46bedd13e858758eb15e819b0aa8f5aa19
Andrew GIBSON <andrewgib14@gmail.com>
2017-08-03 18:03:08 -0500

    created logic for buttons

--- main.js
+++ main.js
@@ -1,10 +1,62 @@
 let buttons = document.querySelectorAll(".key");
+let display = document.querySelector(".display");
+let buttonC = document.querySelector("#numC");
+let equal = document.querySelector("#num--");
+let up = document.querySelector("#arrowUp");
+let down = document.querySelector("#arrowDown");
+let backSpace = document.querySelector("#back");
+let anwser = [];
+let problem = display.innerHTML;
+
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function (e) {
-        var input = document.querySelector('.display');
-        var inputVal = input.innerHTML;
         var btnVal = this.innerHTML;
+        display.innerHTML += btnVal;
     }
 }
+
+buttonC.addEventListener("click", clear)
+
+function clear() {
+    display.innerHTML = "";
+}
+
+equal.addEventListener("click", equals);
+
+function equals() {
+    let sqArray;
+    let sqr;
+    let problem = display.innerHTML;
+    if (problem.indexOf("√") > -1) {
+        let index = problem.indexOf("√");
+        splitProblem = problem.split("");
+        splitProblem.splice(index, 1);
+        sqArray = [];
+        for (let i = index; i < splitProblem.length;) {
+            sqArray.push(splitProblem[i]);
+            if (splitProblem[i + 1] === ")" || i >= splitProblem.length) {
+                i = splitProblem.length;
+            }
+            splitProblem.splice(i, 1);
+        }
+        let num = eval(sqArray.join(""));
+        sqr = Math.sqrt(num);
+        splitProblem.splice(index, 0, sqr);
+        problem = splitProblem.join("");
+    }
+
+    display.innerHTML = eval(problem);
+    anwser.push(display.innerHTML);
+
+}
+
+up.addEventListener("click", upArrow);
+function upArrow() {
+    display.innerHTML = anwser;
+}
+// down.addEventListener();
+// backSpace.addEventListener();
+
+
 
 
